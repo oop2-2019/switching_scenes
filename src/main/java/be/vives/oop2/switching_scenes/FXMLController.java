bb2025abc3caa5c33291efd7c91aa57cccd0acea
Nico De Witte <nico.dewitte@vives.be>
2019-03-12 14:06:41 +0100

    Example of how to switch scenes

---
+++ src/main/java/be/vives/oop2/switching_scenes/FXMLController.java
@@ -0,0 +1,39 @@
+package be.vives.oop2.switching_scenes;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.stage.Stage;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+
+public class FXMLController implements Initializable {
+    
+    @FXML
+    private Label label;
+    
+    @FXML
+    private void handleButtonAction(ActionEvent event) throws Exception {
+        System.out.println("You clicked me!");
+        label.setText("Hello World!");
+
+        // Need stage to change scenes
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        Parent root = FXMLLoader.load(getClass().getResource("/fxml/NewScene.fxml"));
+        Scene newScene = new Scene(root);
+
+        stage.hide(); //optional
+        stage.setScene(newScene);
+        stage.show();        
+    }
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+}
---
+++ src/main/java/be/vives/oop2/switching_scenes/NewSceneController.java
@@ -0,0 +1,13 @@
+package be.vives.oop2.switching_scenes;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+public class NewSceneController implements Initializable {
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+}
